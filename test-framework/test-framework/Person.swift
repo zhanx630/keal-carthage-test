2e7657f29814cc92c0fa719e2ad546883f4157b4
Xinyu Zhang <xinyu.zhang.-nd@disney.com>
2021-07-26 19:41:00 +0800

    test

---
+++ test-framework/test-framework/Person.swift
@@ -0,0 +1,18 @@
+//
+//  Person.swift
+//  test-framework
+//
+//  Created by zhanx630 on 2021/7/26.
+//
+
+import Foundation
+
+class Person {
+    func eat(){
+        print("instance method person eat");
+    }
+    
+    static func eat() {
+        print("calss method person eat");
+    }
+}
---
+++ test-framework/test-framework/test_framework.h
@@ -0,0 +1,18 @@
+//
+//  test_framework.h
+//  test-framework
+//
+//  Created by zhanx630 on 2021/7/26.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for test_framework.
+FOUNDATION_EXPORT double test_frameworkVersionNumber;
+
+//! Project version string for test_framework.
+FOUNDATION_EXPORT const unsigned char test_frameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <test_framework/PublicHeader.h>
+
+
